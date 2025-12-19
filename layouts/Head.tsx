d96410344d6e919093c17172c278d14643373a8b
nikolangelov <nicole.angelov396@gmail.com>
2025-12-19 09:38:43 +0200

    added script for gtm4 in Head

--- layouts/Head.tsx
+++ layouts/Head.tsx
@@ -12,6 +12,15 @@ export default function Head() {
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','GTM-NWKWKC4Z');`}</script>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-DC3NSFR7F5"></script>
+      <script>
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-DC3NSFR7F5');
+        `}
+      </script>
     </>
   );
 }
